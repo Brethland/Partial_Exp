9afe3c73400de8f81ca02afb5c2bccbab0a11f3d
Brethland Yang <ycy1207694344@gmail.com>
2022-04-06 00:00:01 +0200

    TODO: Collide

--- Stau/Auto.cs
+++ Stau/Auto.cs
@@ -14,12 +14,16 @@ namespace Stau
 
         static Random rng = new Random();
 
+        private void Reset(double height, double width)
+        {
+            this.position = (rng.NextDouble() * width, rng.NextDouble() * height);
+            this.speed = (rng.NextDouble() * 40 + 150 * rng.Next(-1, 2), rng.NextDouble() * 40 + 150 * rng.Next(-1, 2));
+            this.accelerate = (rng.NextDouble() * 50 * rng.Next(-1, 2), rng.NextDouble() * 50 * rng.Next(-1, 2));
+        }
         public Auto(double height, double width)
         {
             this.isHalted = false;
-            this.position = (rng.NextDouble() * width, rng.NextDouble() * height);
-            this.speed = (rng.NextDouble() * 40 + 150 * rng.Next(-1, 2), rng.NextDouble() * 40 + 150 * rng.Next(-1, 2));
-            this.accelerate = (rng.NextDouble() * 4 * rng.Next(-1, 2), rng.NextDouble() * 4 * rng.Next(-1, 2));
+            Reset(height, width);
         }
 
         public void Draw(Canvas F)
@@ -43,12 +47,20 @@ namespace Stau
         public void Move(TimeSpan t, Canvas F)
         {
             if(isHalted) return;
-            this.position = (this.position.Item1 + this.speed.Item1 * t.TotalSeconds,
-                            this.position.Item2 + this.speed.Item2 * t.TotalSeconds);
+            //this.position = (this.position.Item1 + this.speed.Item1 * t.TotalSeconds,
+              //              this.position.Item2 + this.speed.Item2 * t.TotalSeconds);
 
-            //this.position = (this.position.Item1 + this.speed.Item1 * t.TotalSeconds + 0.5 * this.accelerate.Item1 * t.TotalSeconds * t.TotalSeconds, 
-              //              this.position.Item2 + this.speed.Item2 * t.TotalSeconds + 0.5 * this.accelerate.Item2 * t.TotalSeconds * t.TotalSeconds);
-            //this.speed = (this.speed.Item1 + this.accelerate.Item1 * t.TotalSeconds, this.speed.Item2 + this.accelerate.Item2 * t.TotalSeconds);
+            this.position = (this.position.Item1 + this.speed.Item1 * t.TotalSeconds + 0.5 * this.accelerate.Item1 * t.TotalSeconds * t.TotalSeconds, 
+                            this.position.Item2 + this.speed.Item2 * t.TotalSeconds + 0.5 * this.accelerate.Item2 * t.TotalSeconds * t.TotalSeconds);
+            this.speed = (this.speed.Item1 + this.accelerate.Item1 * t.TotalSeconds, this.speed.Item2 + this.accelerate.Item2 * t.TotalSeconds);
+
+            if (this.position.Item1 > 2 * Convert.ToDouble(F.ActualWidth)
+                || this.position.Item2 > 2 * Convert.ToDouble(F.ActualHeight))
+            {
+                Reset(Convert.ToInt32(F.ActualHeight), Convert.ToDouble(F.ActualWidth));
+                return;
+            }
+            
             if(this.position.Item1 >= Convert.ToInt32(F.ActualWidth))
             {
                 this.position = (2 * Convert.ToDouble(F.ActualWidth) - this.position.Item1, this.position.Item2);
@@ -69,6 +81,11 @@ namespace Stau
                 this.position = (this.position.Item1, -this.position.Item2);
                 this.speed = (this.speed.Item1, -this.speed.Item2);
             }
+        }
+
+        public void Collide(Auto b)
+        {
+            
         }
 
         public void Acc()
