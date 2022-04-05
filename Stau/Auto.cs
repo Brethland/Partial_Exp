079918e21c52810f1c7cb50574484ef63007766c
Brethland Yang <ycy1207694344@gmail.com>
2022-04-05 14:45:35 +0200

    init

---
+++ Stau/Auto.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Stau
+{
+    class Auto
+    {
+        public ValueTuple<double, double> speed { get; private set; }
+        public ValueTuple<double, double> position { get; private set; }
+        public ValueTuple<double, double> accelerate { get; private set; }
+        public bool isHalted { get; private set; }
+
+        static Random rng = new Random();
+
+        public Auto(double height, double width)
+        {
+            this.isHalted = false;
+            this.position = (rng.NextDouble() * width, rng.NextDouble() * height);
+            this.speed = (rng.NextDouble() * 40 + 150 * rng.Next(-1, 2), rng.NextDouble() * 40 + 150 * rng.Next(-1, 2));
+            this.accelerate = (rng.NextDouble() * 4 * rng.Next(-1, 2), rng.NextDouble() * 4 * rng.Next(-1, 2));
+        }
+
+        public void Draw(Canvas F)
+        {
+            Ellipse e = new Ellipse()
+            {
+                Width = 10,
+                Height = 10,
+                Fill = Brushes.Crimson,
+            };
+            if(position.Item1 + 5 > Convert.ToInt32(F.ActualWidth) 
+                || position.Item1 < 0 
+                || position.Item2 + 5 > Convert.ToInt32(F.ActualHeight) 
+                || position.Item2 < 0)
+                return;
+            Canvas.SetTop(e, position.Item2);
+            Canvas.SetLeft(e, position.Item1);
+            F.Children.Add(e);
+        }
+
+        public void Move(TimeSpan t, Canvas F)
+        {
+            if(isHalted) return;
+            this.position = (this.position.Item1 + this.speed.Item1 * t.TotalSeconds,
+                            this.position.Item2 + this.speed.Item2 * t.TotalSeconds);
+
+            //this.position = (this.position.Item1 + this.speed.Item1 * t.TotalSeconds + 0.5 * this.accelerate.Item1 * t.TotalSeconds * t.TotalSeconds, 
+              //              this.position.Item2 + this.speed.Item2 * t.TotalSeconds + 0.5 * this.accelerate.Item2 * t.TotalSeconds * t.TotalSeconds);
+            //this.speed = (this.speed.Item1 + this.accelerate.Item1 * t.TotalSeconds, this.speed.Item2 + this.accelerate.Item2 * t.TotalSeconds);
+            if(this.position.Item1 >= Convert.ToInt32(F.ActualWidth))
+            {
+                this.position = (2 * Convert.ToDouble(F.ActualWidth) - this.position.Item1, this.position.Item2);
+                this.speed = (-this.speed.Item1, this.speed.Item2);
+            }
+            else if(this.position.Item1 <= 0)
+            {
+                this.position = (-this.position.Item1, this.position.Item2);
+                this.speed = (-this.speed.Item1, this.speed.Item2);
+            }
+            else if(this.position.Item2 >= Convert.ToInt32(F.ActualHeight))
+            {
+                this.position = (this.position.Item1, 2 * Convert.ToDouble(F.ActualHeight) - this.position.Item2);
+                this.speed = (this.speed.Item1, -this.speed.Item2);
+            }
+            else if(this.position.Item2 <= 0)
+            {
+                this.position = (this.position.Item1, -this.position.Item2);
+                this.speed = (this.speed.Item1, -this.speed.Item2);
+            }
+        }
+
+        public void Acc()
+        {
+            this.speed = (this.speed.Item1 * 1.2, this.speed.Item2 * 1.2);
+        }
+        public void DeAcc()
+        {
+            this.speed = (this.speed.Item1 * 0.8, this.speed.Item2 * 0.8);
+        }
+        
+        public void Stop()
+        {
+            if(isHalted == false) this.isHalted = true;
+            else this.isHalted = false;
+        }
+    }
+}
---
+++ Stau/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Stau
+{
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer t = new DispatcherTimer();
+        Queue<Auto> a = new Queue<Auto>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            t.Interval = TimeSpan.FromMilliseconds(17);
+            t.Start();
+            t.Tick += FrameFresh;
+
+            Enumerable.Range(1, 10).ToList().ForEach(i => a.Enqueue(new Auto(Convert.ToInt32(F.Width), Convert.ToInt32(F.Height))));
+        }
+
+        public void FrameFresh(object sender, EventArgs e)
+        {
+            F.Children.Clear();
+            a.ToList<Auto>().ForEach(x => { x.Move(TimeSpan.FromMilliseconds(17), F); x.Draw(F); });
+        }
+
+        public void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Up)) a.ToList<Auto>().ForEach(x => x.Acc());
+            else if (e.Key.Equals(Key.Down)) a.ToList<Auto>().ForEach(x => x.DeAcc());
+            else if (e.Key.Equals(Key.Space)) a.ToList<Auto>().ForEach(x => x.Stop());
+        }
+    }
+}
